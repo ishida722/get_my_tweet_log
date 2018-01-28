7a1453f820e55affa34e67d50576b68cc6c7cabc
chu <they.know.who.is.the.strongest@gmail.com>
2018-01-28 14:55:04 +0900

    ok

--- twapi.py
+++ twapi.py
@@ -3,6 +3,8 @@ import twitter
 import sys
 from datetime import datetime, timezone, timedelta, date
 from EsDataConverter.EsDataConverter import Converter
+from elasticsearch import Elasticsearch
+import os
 
 SCREEN_NAME = 'paragraph_14'
 COUNT = 200
@@ -30,6 +32,11 @@ def HowManyDaysAgo(tweet):
     time = GetDateTime(tweet.created_at).date()
     return (today - time).days
 
+def GetDateStr(created_at_in_seconds):
+    date = datetime.fromtimestamp(created_at_in_seconds, JST)
+    dateStr = date.strftime('%Y%m%dT%H%M%SZ')
+    return dateStr
+
 def GetYesterdayTweets():
     tw = API()
     retTweets = []
@@ -41,11 +48,15 @@ def GetYesterdayTweets():
             days = HowManyDaysAgo(tweet)
             nowEndId = tweet.id
             if days == 1: #Yesteday
-                retTweets.append(tweet)
+                retTweets.append({'usr':tweet.user.screen_name, 'date':GetDateStr(tweet.created_at_in_seconds), 'text':tweet.text})
             elif days > 1:
                 isTweetTodayOrYesterday = False
                 break
     return retTweets
 
 if __name__ == '__main__':
-    print(GetYesterdayTweets())
+    tweets = GetYesterdayTweets()
+    es  = Elasticsearch(os.getenv('ES_SERVER', 'http://localhost:9200'))
+    for tweet in tweets:
+        res = es.index(index='twitter', doc_type='my_timeline', body=tweet)
+        print(res)
