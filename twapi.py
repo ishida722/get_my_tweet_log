056d736339b09d4f0c32571cc7c821bd3fd0b86e
chu <they.know.who.is.the.strongest@gmail.com>
2018-02-06 10:13:23 +0900

    add func that select date

--- twapi.py
+++ twapi.py
@@ -27,7 +27,10 @@ def GetDateStr(created_at_in_seconds):
     dateStr = date.strftime('%Y-%m-%dT%H:%M:%S+09:00')
     return dateStr
 
-def GetYesterdayTweets():
+YESTERDAY = 1
+TODAY = 0
+
+def GetTweets(day):
     tw = API()
     retTweets = []
     nowEndId = None
@@ -37,23 +40,23 @@ def GetYesterdayTweets():
         for tweet in rawResult:
             days = HowManyDaysAgo(tweet)
             nowEndId = tweet.id
-            if days == 1 or tweet.retweeted == True : #Yesteday
+            if days == day or tweet.retweeted == True : #Yesteday
                 retTweets.append({'user':tweet.user.screen_name, 'date':GetDateStr(tweet.created_at_in_seconds), 'text':tweet.text})
-            elif days > 1:
+            elif days > day:
                 isTweetTodayOrYesterday = False
                 break
     return retTweets
 
-def IndexYesterdayTweets():
-    tweets = GetYesterdayTweets()
+def IndexTweets(day):
+    tweets = GetTweets(day)
     es  = Elasticsearch(os.getenv('ES_SERVER', 'http://localhost:9200'))
     for tweet in tweets:
         res = es.index(index='twitter', doc_type='tweet', body=tweet)
         print(res)
     return res
 
-def PrintYesterdayTweet():
-    tweets = GetYesterdayTweets()
+def PrintTweet(day):
+    tweets = GetTweets(day)
     for tw in tweets:
         print('{} : {} : {}'.format(tw['user'], tw['text'], tw['date']))
 
@@ -61,9 +64,13 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='Get tweetlog')
     parser.add_argument('-p', '--print', action='store_true',  help='print only')
+    parser.add_argument('-d', '--day', default=YESTERDAY,  help='day')
     args = parser.parse_args()
 
+    day = YESTERDAY
+    print(args.day)
+
     if args.print: # -p --print option
-        PrintYesterdayTweet()
+        PrintTweet(day)
     else:
-        IndexYesterdayTweets()
+        IndexTweets(day)
